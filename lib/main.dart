fd12c09045a0e2e246b2a40a9e06295d67311690
minhphan46 <21522345@gm.uit.edu.vn>
2023-02-23 23:14:17 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:great_places/screens/places_list_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+        accentColor: Colors.amber,
+      ),
+      home: const PlacesListScreen(),
+    );
+  }
+}
---
+++ lib/screens/place_detail_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PlaceDetailScreen extends StatelessWidget {
+  const PlaceDetailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
